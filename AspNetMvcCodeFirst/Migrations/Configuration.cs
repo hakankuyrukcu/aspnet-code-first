29d0c6a269f69421378c60aff473cb4a13ad5f5a
Hakan KUYRUKÇU <hakankuyrukcu@gmail.com>
2019-08-26 19:02:43 +0300

    Added new comments

--- AspNetMvcCodeFirst/Migrations/Configuration.cs
+++ AspNetMvcCodeFirst/Migrations/Configuration.cs
@@ -14,18 +14,8 @@ namespace AspNetMvcCodeFirst.Migrations
 
         protected override void Seed(AspNetMvcCodeFirst.Models.CodeFirstContext context)
         {
-            //  This method will be called after migrating to the latest version.
-
-            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
-            //  to avoid creating duplicate seed data. E.g.
-            //
-            //    context.People.AddOrUpdate(
-            //      p => p.FullName,
-            //      new Person { FullName = "Andrew Peters" },
-            //      new Person { FullName = "Brice Lambson" },
-            //      new Person { FullName = "Rowan Miller" }
-            //    );
-            //
+            // Specific data is added to the table each time the database is updated
+            // Because the product id is the primary key, it will not add if the data exists in the table.
 
             context.Products.AddOrUpdate(
                 new Models.Products { ProductID = 1, ProductName = "Book" },
--- AspNetMvcCodeFirst/Models/CodeFirstContext.cs
+++ AspNetMvcCodeFirst/Models/CodeFirstContext.cs
@@ -14,11 +14,21 @@ namespace AspNetMvcCodeFirst.Models
             Database.SetInitializer<CodeFirstContext>(null);
         }
 
+        /// <summary>
+        /// In the constructor method, we specify the connection string to be based on.
+        /// This connection string created on web.config file.
+        /// </summary>
         public CodeFirstContext() : base("Name=CodeFirstConnection")
         {
         }
 
+        // All tables declared for Seed Method in Migrations/Configuration.cs
         public DbSet<Products> Products { get; set; }
+
+        /// <summary>
+        /// All tables properties set for model creating
+        /// </summary>
+        /// <param name="modelBuilder"></param>
         protected override void OnModelCreating(DbModelBuilder modelBuilder)
         {
             modelBuilder.Configurations.Add(new ProductsTable());
